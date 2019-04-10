c33a1daa52debe90edd9dc04caf99229ca0fa1c5
ghost <ghost@users.noreply.github.com>
2019-04-10 13:13:39 +0900

    test push

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World\nTeam Sylphrena");
-
+        System.out.println("test push");
+        //test push
     }
 }
