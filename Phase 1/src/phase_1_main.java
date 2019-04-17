e6b60e63b0e98e2edb9fc93c27a53164c7a4338c
Wen Wen <sonicwen2004@gmail.com>
2019-04-17 13:15:44 +0900

    create phase 1

--- Phase 1/src/phase_1_main.java
+++ Phase 1/src/phase_1_main.java
@@ -1,7 +1,18 @@
-public class phase_1_main {
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World\nTeam Sylphrena");
+import java.io.FileNotFoundException;
 
+public class phase_1_main {
+
+    phase_1_main(){
+        data InputData;
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // write your code here
+        //data InputData;
+        System.out.println("Hello World\nTeam Sylphrena");
+        System.out.println(args[0]);
+        new readInput(args[0]);
     }
 }
