0d01601008eb74fffcce3bdeaab6e3b93c03bbd0
ghost <ghost@users.noreply.github.com>
2019-04-17 13:15:44 +0900

    create phase 1

---
+++ Phase 1/src/data.java
@@ -0,0 +1,16 @@
+class data {
+    private int N, M, P, Q;
+    private double[]points;
+    private int[]connections;
+
+    data(int N, int M, int P, int Q){
+        this.N = N;
+        this.M = M;
+        this.P = P;
+        this.Q = Q;
+        points = new double[N];
+        connections = new int[M];
+    }
+
+
+}
