4ecbd7f5f912a6d51192900f99f4c3b5e7cd1744
Wen Wen <sonicwen2004@gmail.com>
2019-04-24 00:59:23 +0900

    create phase 1 Task 2 completed

--- Phase 1/src/phase_1_main.java
+++ Phase 1/src/phase_1_main.java
@@ -1,10 +1,7 @@
-import java.io.FileNotFoundException;
-
 public class phase_1_main {
 
     public static void main(String[] args) {
         DataClass InputData = new DataClass(args[0]);
         InputData.printAllCrossPoint();
-
     }
 }
