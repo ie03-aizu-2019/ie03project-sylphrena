86d4e64f3ac93023e385c60192668a61801023e5
Wen Wen <sonicwen2004@gmail.com>
2019-04-24 00:50:09 +0900

    create phase 1 Task 2 completed

--- Phase 1/src/phase_1_main.java
+++ Phase 1/src/phase_1_main.java
@@ -2,17 +2,9 @@ import java.io.FileNotFoundException;
 
 public class phase_1_main {
 
-    phase_1_main(){
-        data InputData;
+    public static void main(String[] args) {
+        DataClass InputData = new DataClass(args[0]);
+        InputData.printAllCrossPoint();
 
-    }
-
-    public static void main(String[] args) throws FileNotFoundException {
-
-        // write your code here
-        //data InputData;
-        System.out.println("Hello World\nTeam Sylphrena");
-        System.out.println(args[0]);
-        new readInput(args[0]);
     }
 }
